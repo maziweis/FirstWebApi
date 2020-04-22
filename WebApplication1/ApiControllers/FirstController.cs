05fef31cfc0541f166f626fcf6caaadec00011dd
maziweis <201512895@qq.com>
2020-04-22 12:14:29 +0800

    1

---
+++ WebApplication1/ApiControllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.ApiControllers
+{
+    public class FirstController : ApiController
+    {
+        [HttpGet]
+        public int getFirst(int id)
+        {
+            return id;
+        }
+        [HttpPost]
+        public string PostFirst(string name,string sex)
+        {
+            return "姓名："+name+" 性别：" + sex;
+        }
+    }
+}
